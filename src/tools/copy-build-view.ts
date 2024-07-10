85629614526bff8ccff4dcdec13c8d5edc3f7baf
alancrps <alanfcorpus@gmail.com>
2024-07-10 14:07:35 -0300

    test

--- src/tools/copy-build-view.ts
+++ src/tools/copy-build-view.ts
@@ -1,5 +1,5 @@
 import * as shell from 'shelljs';
 
 // copia todos las vistas
-shell.cp('-R', 'src/views', '/opt/render/project/src/dist/src/views');
-shell.cp('-R', 'public', '/opt/render/project/src/dist/public');
+shell.cp('-R', 'src/views', 'dist/src/views');
+shell.cp('-R', 'public', 'dist/public');
