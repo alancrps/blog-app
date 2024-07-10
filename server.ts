f236a7136a3584605bb6f5b59d7382a80a53c849
alancrps <alanfcorpus@gmail.com>
2024-07-10 02:47:11 -0300

    fix

--- server.ts
+++ server.ts
@@ -58,7 +58,7 @@ app.use('/', (req: Request, res: Response) => {
 	res.redirect('/noticias')
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
 	console.log(`Servidor Express funcionando en http://localhost:${port} 🚀✅`);
 });
