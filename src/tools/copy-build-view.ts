9fe03b3979eb41bbf686e0e090f51cf4d33b88c1
alancrps <alanfcorpus@gmail.com>
2024-07-10 13:58:21 -0300

    test

--- src/tools/copy-build-view.ts
+++ src/tools/copy-build-view.ts
@@ -1,5 +1,11 @@
 import * as shell from 'shelljs';
 
-// copia todos las vistas
-shell.cp('-R', 'src/views', 'dist/src/views');
-shell.cp('-R', 'public', 'dist/public');
+// Crear los directorios necesarios
+shell.mkdir('-p', 'dist/src/views');
+shell.mkdir('-p', 'dist/public');
+
+// Copiar archivos individuales de 'src/views' a 'dist/src/views'
+shell.cp('-R', 'src/views/*', 'dist/src/views');
+
+// Copiar archivos individuales de 'public' a 'dist/public'
+shell.cp('-R', 'public/*', 'dist/public');
